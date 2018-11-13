c3c89090c95a1ce24a6c31835faf4626b8df20e9
cihangirgokoz <chngrgkz@gmail.com>
2018-11-13 20:52:03 +0300

    bazı düzenlemeler yapıldı

--- src/main/java/com/robusta/excelread/controller/ExcelController.java
+++ src/main/java/com/robusta/excelread/controller/ExcelController.java
@@ -1,26 +1,22 @@
 package com.robusta.excelread.controller;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.springframework.stereotype.Component;
-
 import com.robusta.excelread.constant.ActionType;
 import com.robusta.excelread.dao.Excel;
 import com.robusta.excelread.function.RobustaFunctions;
 import com.robusta.excelread.model.JsonResponse;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Component
 @Path("/excel")
-@Api(value = "/excel") // Enables Swagger Documentation
+@Api(value = "/excel")
 @Produces({MediaType.APPLICATION_JSON})
 @Consumes(MediaType.APPLICATION_JSON)
 public class ExcelController {
@@ -28,7 +24,6 @@ public class ExcelController {
 	
 	@POST
     @Path("/")
-	// success response
     @ApiOperation(value = "Excel Service Operation")
     public JsonResponse excel(Excel excel) {
 		if((excel.getAction().isEmpty() && 
@@ -45,7 +40,7 @@ public class ExcelController {
 			return new JsonResponse("success",robustaFunctions.excelOperations(excel));
 		} catch (Exception e) {
 			e.printStackTrace();
-			return new JsonResponse("failed",e.toString());
+			return new JsonResponse("failed",e.getMessage().toString());
 		}
     }
 
--- src/main/java/com/robusta/excelread/function/RobustaFunctions.java
+++ src/main/java/com/robusta/excelread/function/RobustaFunctions.java
@@ -3,75 +3,83 @@ package com.robusta.excelread.function;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Comment;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.ss.util.CellReference;
-import org.springframework.core.io.ClassPathResource;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.robusta.excelread.dao.Excel;
 
 public class RobustaFunctions {
 
-	public String excelOperations(Excel excel)
-			 throws Exception{
+    public String excelOperations(Excel excel)
+            throws Exception {
 
-		if (!excel.action.isEmpty() && !excel.filePath.isEmpty() && excel.rowIndex != null && excel.colIndex != null) {
-			String response;
-			Workbook workbook = null;
-			File file;
-			FileOutputStream fileOut;
-			FileInputStream fileInputStream;
-			OPCPackage oPackage;
-			try {
-				 file = new ClassPathResource(excel.filePath).getFile();
-				 fileInputStream = new FileInputStream(file);
-				 oPackage = OPCPackage.open(fileInputStream);
-				workbook = WorkbookFactory.create(oPackage);
+        if (!excel.action.isEmpty() && !excel.filePath.isEmpty() && excel.rowIndex != null && excel.colIndex != null) {
 
-				Sheet sheet = workbook.getSheetAt(0);
-				Row row = sheet.getRow(excel.rowIndex);
-				Cell cell = row.getCell(excel.colIndex);
+            String response;
+            Workbook workbook = null;
+            File file = null;
+            FileOutputStream fileOut = null;
+            FileInputStream fileInputStream;
+            OPCPackage oPackage;
 
-				switch (excel.action.toUpperCase()) {
-				case "GETCELLDATA":
+            try {
+                file = new File(excel.filePath);
+                fileInputStream = new FileInputStream(file);
+                oPackage = OPCPackage.open(fileInputStream);
+                workbook = WorkbookFactory.create(oPackage);
 
-					response =  cell.getStringCellValue();
-					break;
+                Sheet sheet = workbook.getSheetAt(0);
+                Row row = sheet.getRow(excel.rowIndex);
+                Cell cell = null;
 
-				case "SETCELLDATA":
-					
-					cell.setCellValue(excel.getNewValue().toString());	
-					fileOut  = new FileOutputStream(file);
-					workbook.write(fileOut);
-					workbook.close();
-					fileOut.close();
-					response =  "set new value  :"+ excel.getNewValue()+" to row :" + excel.getRowIndex() + " cell :" + excel.getColIndex() + "";
-					break;
-				default:
-					return "this action is not available";
-				}
 
-			} catch (Exception e) {
-				throw new Exception(e);
-			}finally {
-				if(workbook != null)
-					workbook.close();
-			}
+                switch (excel.action.toUpperCase()) {
+                    case "GETCELLDATA":
+                        if(row == null)
+                            throw new Exception("invalid rows");
 
-			
-			return response;
-		}
+                        cell = row.getCell(excel.colIndex);
 
-		return "please enter required parameter";
-	}
+                        if(cell == null)
+                            throw new Exception("invalid cells");
+
+                        response = cell.getStringCellValue();
+                        break;
+                    case "SETCELLDATA":
+
+                        if (row == null)
+                            row = sheet.createRow(excel.rowIndex);
+
+                        cell = row.getCell(excel.colIndex);
+
+                        if(cell == null)
+                            cell = row.createCell(excel.colIndex);
+
+                        cell.setCellValue(excel.getNewValue().toString());
+                        fileInputStream.close();
+                        fileOut = new FileOutputStream(file);
+                        workbook.write(fileOut);
+                        workbook.close();
+                        fileOut.close();
+                        response = "set new value  :" + excel.getNewValue() + " to row :" + excel.getRowIndex() + " cell :" + excel.getColIndex() + "";
+                        break;
+                    default:
+                        throw new Exception("This action is not available");
+                }
+            } catch (Exception e) {
+                if(workbook != null)
+                    workbook.close();
+                if(fileOut != null)
+                    fileOut.close();
+                throw e;
+            }
+            return response;
+        }
+        throw new Exception("Please enter required parameter");
+    }
 }
